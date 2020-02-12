e3c6bbd433f55f573073b93625de0edf467bee20
LCN29 <CanLee529@gmail.com>
2020-02-12 19:58:47 +0800

    alipay config

--- payment-web/src/main/java/com/lcn29/PaymentApplicationLauncher.java
+++ payment-web/src/main/java/com/lcn29/PaymentApplicationLauncher.java
@@ -1,5 +1,6 @@
 package com.lcn29;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@MapperScan("com.lcn29.mapper")
 public class PaymentApplicationLauncher {
 
     public static void main(String[] args) {
